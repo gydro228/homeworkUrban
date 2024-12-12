6d00cbeab6342575a2fba201c86786812d9d74cb
gydro228 <128089215+gydro228@users.noreply.github.com>
2024-12-12 11:57:09 +0300

    Update Домашнее задание по теме "Зачем нужно наследование".py

--- Домашнее задание по теме "Зачем нужно наследование".py
+++ Домашнее задание по теме "Зачем нужно наследование".py
@@ -4,54 +4,52 @@ class Animal:
         self.alive = True
         self.fed = False
 
+    def eat(self, food):
+        if type(food) == Plant:  
+            if food.edible:
+                print(self.name, 'съел', food.name)
+                self.fed = True
+            else:
+                print(self.name, 'не стал есть', food.name)
+                self.alive = False
+        else:
+            print(self.name, 'не притронулся к', food.name)
 
 class Plant:
     def __init__(self, name):
         self.name = name
         self.edible = False
-class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(self.name,'съел', food.name)
-            self.fed = True
-        else:
-            print(self.name,'не стал есть', food.name)
-            self.alive = False
 
+class Mammal(Animal):
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(self.name,'съел', food.name)
-            self.fed = True
-        else:
-            print(self.name,'не стал есть', food.name)
-            self.alive = False
-
+    pass
 
 class Flower(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-
+    pass
 
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
         self.edible = True
 
+
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
+
 
 print(a1.name)
 print(p1.name)
 print(a1.alive)
 print(a2.fed)
 
+
 a1.eat(p1)
 a2.eat(p2)
 
+
 print(a1.alive)
 print(a2.fed)
-
